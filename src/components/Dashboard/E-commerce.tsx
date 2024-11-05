dff53f0e7f5b6503a44fa1b6c9df30ad4f129b2b
Satyarshi Shukla <satyarshishukla123@gmail.com>
2024-11-05 12:08:21 +0530

    initial ui

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -8,7 +8,7 @@ import CardDataStats from "../CardDataStats";
 const ECommerce: React.FC = () => {
   return (
     <>
-      <p className="text-2xl font-bold text-black dark:text-white py-5">This Week's Overview</p>
+      <p className="text-2xl font-bold text-black dark:text-white py-5">This Week&#39;s Overview</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
         <CardDataStats title="Total Clients" total="590" rate="+2.5%" levelUp />
           
